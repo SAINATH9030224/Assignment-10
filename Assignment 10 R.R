cd1cc618f6de4644f708ffd184117b81b4614234
SAINATH9030224 <guddetir@usf.edu>
2024-11-03 21:44:47 -0500

    Update Assignment 10 R.R

--- Assignment 10 R.R
+++ Assignment 10 R.R
@@ -1,7 +1,7 @@
 Package: Sainath
 Title: Analyze and Visualize DNA Sequences
 Version: 0.1
-Authors@R:("RamSainathReddy", "Guddeti", "guddetir@usf.edu, role = c("aut", "cre")),
+Authors@R:("RamSainathReddy", "Guddeti", "guddetir@usf.edu, role = c("aut", "cre"))
 Description: The Sainath package provides tools to analyze DNA sequences,
 including functions to calculate the frequency of each nucleotide (A, T, C, G)
 within a sequence, sequence length,and nucleotide frequencies. It also includes 
